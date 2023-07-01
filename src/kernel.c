58fa8208a837d41cf6719ab4ecf3ea6734312041
clee01 <1726241244@qq.com>
2023-07-01 16:07:28 +0800

    learn: 变更paging相关函数签名

--- src/kernel.c
+++ src/kernel.c
@@ -123,7 +123,7 @@ void kernel_main() {
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
 
     // Switch to kernel paging chunk
-    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+    paging_switch(kernel_chunk);
 
     // char* ptr = kzalloc(4096);
     // paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
